24c00e67e6aa732f36d5066a90c1fdc4146b3433
Kamil Kulak <kulak.kam@gmail.com>
2017-01-07 12:43:59 +0100

    Scheduler, readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Getting started
+
+```$ export AWS_ACCESS_KEY_ID="anaccesskey"```
+```$ export AWS_SECRET_ACCESS_KEY="asecretkey"```
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import time
+import schedule
 from db import DB
 from rekognition_service import detect_labels
 from serializer import serialize
 
-if __name__ == '__main__':
-    
-    db = DB()
-    db.store(time.time(), detect_labels())
+def detect_and_store_labels(db, path):
+    def __detect_and_store_labels():
+        db.store(time.time(), detect_labels())
+        serialize(db, path)
+    return __detect_and_store_labels
 
-    serialize(db, 'data.txt')
+if __name__ == '__main__':
+    db = DB()
+    schedule.every(1).minutes.do(detect_and_store_labels(db, 'data.txt'))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
