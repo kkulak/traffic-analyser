5e8f8ab96ab7497be4817b4ef80ac7f59c88d7e6
novy <slnowak154@gmail.com>
2017-01-07 14:22:31 +0100

    Hack for absolute/relative photo url

--- fetch_images.py
+++ fetch_images.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 
 
 def take_snapshot_from_camera(camera_name):
+    base_url = 'http://www.gddkia.gov.pl'
+    cameras_suffix = '/pl/22'
+
     return _get_image(
         _direct_link_to_image(
-            _direct_link_to_camera(camera_name=camera_name,
-                                   base_url='http://www.gddkia.gov.pl',
-                                   all_cameras_suffix='/pl/22')
+            link_to_camera=_direct_link_to_camera(camera_name=camera_name,
+                                                  base_url=base_url,
+                                                  all_cameras_suffix=cameras_suffix),
+            base_url=base_url
         )
     )
 
@@ -19,11 +23,12 @@ def _direct_link_to_camera(camera_name, base_url, all_cameras_suffix):
     return base_url + camera_anchor['href']
 
 
-def _direct_link_to_image(link_to_camera):
+def _direct_link_to_image(link_to_camera, base_url):
     camera_page = requests.get(link_to_camera)
     as_soup = BeautifulSoup(camera_page.text, "html.parser")
     img_element = as_soup.find('img', {'class': 'camera'})
-    return img_element['src']
+    absolute_url_or_suffix = img_element['src']
+    return absolute_url_or_suffix if absolute_url_or_suffix.startswith('http') else base_url + absolute_url_or_suffix
 
 
 def _get_image(url):
